506a7fd45ce5126a18414acdbd8854cfa5e44f27
mKnauer <max.knauer@gmx.de>
2020-04-28 18:13:46 +0200

    aktuelle Version

--- src/sample/Aufgabenfenster.java
+++ src/sample/Aufgabenfenster.java
@@ -4,11 +4,14 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
@@ -25,9 +28,9 @@ public class Aufgabenfenster {
     //Setup Table
 
         TableView<Person>klassenTable = new TableView<Person>();
+
         klassenTable.setMaxWidth(300);
         klassenTable.setMaxHeight(300);
-
 
         TableColumn<Person,String> firstNameCol = new TableColumn<Person,String>("Vorname");
         TableColumn<Person, String> lastNameCol = new TableColumn<Person, String>("Nachname");
@@ -44,7 +47,29 @@ public class Aufgabenfenster {
                 klassenTable.setItems(newValue.getKlassenListe());
             }
         });
-    //
+
+
+        //Daten in Tableview Editierbar und in Klassenliste speichern
+        klassenTable.setEditable(true);
+
+        firstNameCol.setCellFactory(TextFieldTableCell.<Person>forTableColumn());
+
+        firstNameCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> event) {
+               String newValue = event.getNewValue();
+               TablePosition<Person, String>position = event.getTablePosition();
+
+                System.out.println(newValue + " " + position.getColumn() + " " + position.getRow());
+
+                Person person = event.getTableView().getItems().get(position.getRow());
+                person.setFirstName(newValue);
+            }
+        });
+
+
+
+
         aufgabenBox.getChildren().addAll(klassenCB, klassenTable);
 
         return aufgabenBox;
